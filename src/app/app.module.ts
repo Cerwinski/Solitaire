685f6f93656e64267ed49a31c68d73a12c57771a
AKQUINET\mcerwinski <nomoger233@gmail.com>
2023-05-12 16:46:39 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { KartenComponent } from './karten/karten.component';
-import { SpielfeldComponent } from './spielfeld/spielfeld.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
@@ -12,8 +12,8 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 @NgModule({
   declarations: [
     AppComponent,
-    KartenComponent,
-    SpielfeldComponent
+    KartenComponent
+
   ],
   imports: [
     BrowserModule,
