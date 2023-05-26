042ac74293b00a4cb76343e7f0fa791591b7bebc
mcerwinski <maximilian.cerwinski@akquinet.de>
2023-05-26 16:39:44 +0200

    more improvements

--- src/app/karten/karten.component.ts
+++ src/app/karten/karten.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { fromEvent, map, startWith } from 'rxjs';
 import { DeckManagementService } from '../shared/services/deck-management.service';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -10,6 +10,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
 })
 export class KartenComponent {
   card?: number;
+  @ViewChild('cardelement') element? : ElementRef;
+
 
   readonly mousePosition$ = fromEvent<MouseEvent>(document, 'mousemove').pipe(
     map(event => ({x: event.clientX, y: event.clientY}))
@@ -18,10 +20,22 @@ export class KartenComponent {
 
   constructor(public deckService: DeckManagementService) {
     console.log('deck', deckService.cDeck());
+    this.mousePosition$.subscribe(e =>{
+      if(this.element?.nativeElement){
+        console.log("Element ist da", this.element.nativeElement);
+        this.element.nativeElement.style.left = e.x + 'px' ;
+        this.element.nativeElement.style.top = e.y + 'px';
+      }
+      else{
+        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+      }
+    })
   }
 
   readonly windowWidth$ = fromEvent(window, 'resize').pipe(
     map(event => (event?.target as Window).innerWidth),
     startWith(window.innerWidth)
   );
+
+
 }
--- src/app/shared/services/deck-management.service.ts
+++ src/app/shared/services/deck-management.service.ts
@@ -2,7 +2,7 @@ import { computed, Injectable, signal } from '@angular/core';
 
 Injectable();
 export class DeckManagementService {
-  private sDeck = signal([1, 2, 3]);
+  private sDeck = signal([1, 2, 3,2,1,2,1]);
   public cDeck = computed(() => {
     console.log('updated deck', this.sDeck());
     return this.sDeck();
